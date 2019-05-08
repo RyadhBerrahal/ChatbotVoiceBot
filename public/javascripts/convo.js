95b33b47151f9856de6a17b1752235c15940b64d
Unknown <ryadh.berrahal@esprit.tn>
2019-05-08 13:06:19 +0100

    s7

--- public/javascripts/convo.js
+++ public/javascripts/convo.js
@@ -1,5 +1,5 @@
 var botui = new BotUI('api-bot');
-var socket = io.connect('https://damp-plateau-30009.herokuapp.com:8010');
+var socket = io.connect('https://damp-plateau-30009.herokuapp.com:3000');
 // read the BotUI docs : https://docs.botui.org/
 
 
